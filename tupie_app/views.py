50c818248b2ce43168c82cc5596eb58ba3f54f84
msomea <msomearaphael@gmail.com>
2025-07-12 22:37:12 +0300

    Added Signup view in tupie_app view.py

---
+++ tupie_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+def home(request):
+    return render(request, 'tupie_app/home.html')
+
+def signup(request):
+    if request
