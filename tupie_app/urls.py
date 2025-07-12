bd0f0f787e4891aca63b5d70397abaf5f84c5491
msomea <msomearaphael@gmail.com>
2025-07-12 23:12:56 +0300

    Added Templates for tupie_app home.html, login.htmlsignup.html

--- tupie_app/urls.py
+++ tupie_app/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 app_name = 'tupie_app'
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    #path('login/', views.user_login, name='login'),
+    #path('logout/', views.user_logout, name='logout'),
 ]
--- tupie_app/views.py
+++ tupie_app/views.py
@@ -7,4 +7,12 @@ def home(request):
     return render(request, 'tupie_app/home.html')
 
 def signup(request):
-    if request
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account Created! Please log in.')
+            return redirect('tupie_app:login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'tupie_app/signup.html', {'form': form})
