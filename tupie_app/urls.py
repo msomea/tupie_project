45fb7b0856e6e5fef60acc8fd40435fb0101ec01
msomea <msomearaphael@gmail.com>
2025-07-13 20:55:06 +0300

    Added static\tupie_app folders to add style.css and add different stles for pages

--- tupie_app/urls.py
+++ tupie_app/urls.py
@@ -5,6 +5,7 @@ app_name = 'tupie_app'
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
-    #path('login/', views.user_login, name='login'),
-    #path('logout/', views.user_logout, name='logout'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('add_item/', views.add_item, name='add_item'),
 ]
